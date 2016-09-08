71a699eb43c07e71ff502c96226f95c127e041c7
Ronesha Dennis <roneshadennis@admins-MacBook-Air.local>
2016-09-08 19:23:49 -0400

    updates to show track listing

--- spotify.py
+++ spotify.py
@@ -1,8 +1,13 @@
 # Get the user's input as the artist's name.
 # Return the artist's albums.
 
+import sys
 import urllib2
 import json
+import pprint
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 print "Find the albums of the artists you love on Spotify!"
 
@@ -21,20 +26,50 @@ while Continue == "yes" or Continue == "y":
 
 			while True:
 				try:
-					apiCall = urllib2.urlopen("https://api.spotify.com/v1/artists/" + artistID + "/albums")
+					apiCall = urllib2.urlopen("https://api.spotify.com/v1/artists/" + artistID + "/albums?market=US&album_type=album&limit=50")
 					apiRead = apiCall.read()
 					artistAlbumData = json.loads(apiRead)
 					apiCall.close()
 
 					artistAlbums = []
+					albumSelectCounter = 1
+					albumNumber = 0 
 
 					for x in artistAlbumData["items"]:
-						if x["name"] not in artistAlbums:
-							if "US" in x["available_markets"]:
-								artistAlbums.append(x["name"])
-								print x["name"]
+						albumNumber += 1
+						artistAlbums.append([{"ourID": albumSelectCounter}, {"name": x["name"]}, {"spotifyID": x["id"]}])
+						albumSelectCounter += 1
+
+					print("This artist has %s albums available on Spotify for the U.S. market. \n" %albumNumber)
+
+					if albumNumber >= 1: 
+						for albums in artistAlbums:
+							print str(albums[0]["ourID"]) + ". " + str(albums[1]["name"])
+
+						while True: 
+							try: 
+								albumSection = input("Enter the number that corresponds with the album track list you'd like to view: ")
+								for albums in artistAlbums:
+									if albumSection == albums[0]["ourID"]:
+										spotifyID = albums[2]["spotifyID"]
+										chosenAlbum = albums[1]["name"]
+										apiCall = urllib2.urlopen("https://api.spotify.com/v1/albums/" + spotifyID + "/tracks")
+										apiRead = apiCall.read()
+										albumTrackData = json.loads(apiRead)
+										apiCall.close()
+
+										print("%s Track List" %chosenAlbum)
+
+										for track in albumTrackData["items"]:
+											print str(track["track_number"]) + ". " + str(track["name"])
+								break
+									
+							except TypeError:
+								print "Invalid entry."
+								break
+
 					break
-				except:
+				except IndexError:
 					print ("This artist does not have a catalog of albums on Spotify.")
 					break
 			break
